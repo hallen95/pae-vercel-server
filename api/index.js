507bc9b95d825a9b08582e6be05d55d10e3dd925
BrianBitopia <brian@bitopia.digital>
2022-10-23 16:41:55 -0300

    rewrite

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('../routes');
+const routes = require('./routes');
 const mongoString = 'mongodb+srv://hallen95:PJzhWJTmH1bX7VWL@cluster0.qcfyunr.mongodb.net/test';
 
 mongoose.connect(mongoString);
