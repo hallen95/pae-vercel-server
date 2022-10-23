20820bc9c9779f933cf6ccca91c278f5efde8bcb
BrianBitopia <brian@bitopia.digital>
2022-10-23 15:52:00 -0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+const mongoString = process.env.DATABASE_URL || 'mongodb + srv://hallen95:PJzhWJTmH1bX7VWL@cluster0.qcfyunr.mongodb.net/test';
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+const app = express();
+
+app.use(express.json());
+// 
+app.use('/api', routes)
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(4000, () => {
+    console.log(`Server Started at ${4000}`)
+})
